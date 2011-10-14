6d4538a61e301e5458329fb6a3d21e2c455a0dac
Harm Aarts <harmaarts@gmail.com>
2011-10-14 21:05:50 +0200

    steady headway into making flipcoin work, added datastore, formalized form inputs

--- flipco.in/flipcoin.go
+++ flipco.in/flipcoin.go
@@ -2,10 +2,10 @@ package flipcoin
 
 import (
     "fmt"
-    /*"appEngine"*/
+    "appEngine"
     "appengine/datastore"
     "http"
-    /*"time"*/
+    "time"
     "mustache.go"
 )
 
@@ -13,6 +13,7 @@ type Coinflip struct {
   Participants []Participant
   Head         string
   Tail         string
+  Done         bool
 }
 
 type Participant struct {
@@ -27,15 +28,53 @@ func init() {
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprint(w, mustache.RenderFile("./flipco.in/views/home.html", map[string]string{"title":"Awesome coin tosses - Flipco.in", "nr_of_flips":"123"}))
+  c := appengine.NewContext(r)
+  count, err := datastore.NewQuery("Coinflip").Count(c)
+  if err != nil {
+    http.Error(w, err.String(), http.StatusInternalServerError)
+    return
+  }
+  fmt.Println(count)
+  fmt.Fprint(w, mustache.RenderFile("./flipco.in/views/home.html", map[string]string{"title":"Awesome coin tosses - Flipco.in", "nr_of_flips":fmt.Sprint(count)}))
 }
 
 func create(w http.ResponseWriter, r *http.Request) {
   if r.Method != "POST" {
     http.Redirect(w, r, "/", 302)
+    /*return*/
   }
 
+  c := appengine.NewContext(r)
+
   r.ParseForm()
+  tail    := r.Form["tail"][0]
+  head    := r.Form["head"][0]
+  friends := r.Form["friends"]
+
+  /*if tail == "" || head == "" || friends == nil {*/
+    /*http.Redirect(w, r, "/", 302)*/
+    /*return*/
+  /*}*/
+
+  participants := make([]Participant, len(friends))
+  for i := range friends {
+    participants[i] = Participant{Email: friends[i], Seen: datastore.SecondsToTime(time.Seconds())}
+  }
+
+  coin := Coinflip {
+    Head: head,
+    Tail: tail,
+    Participants: participants,
+    Done: false,
+  }
+
+  _, err := datastore.Put(c, datastore.NewIncompleteKey(c, "Coinflip", nil), &coin)
+  if err != nil {
+    http.Error(w, err.String(), http.StatusInternalServerError)
+    return
+  }
+
+
   fmt.Println(r.Form)
   for k, v := range r.Form {
     fmt.Println(k)
@@ -44,6 +83,7 @@ func create(w http.ResponseWriter, r *http.Request) {
       fmt.Println(sv)
     }
   }
+
   http.Redirect(w, r, "/show/test_hash", 302)
 }
 
